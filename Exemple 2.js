8883c5f41ecc75775076ae05414d1ff3dee628ff
ZestAgency <discord.strategyfr@gmail.com>
2024-07-09 20:42:10 +0200

    Delete Exemple 2.js

--- Exemple 2.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
-client.once('ready', () => {
-    console.log('Bot is online!');
-});
-
-client.login(process.env.DISCORD_TOKEN);
