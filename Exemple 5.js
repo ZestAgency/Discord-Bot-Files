c1fb393c7e7420e9c4e59ef33f2ef00e9ba85f0a
ZestAgency <discord.strategyfr@gmail.com>
2024-07-09 20:42:46 +0200

    Delete Exemple 5.js

--- Exemple 5.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
