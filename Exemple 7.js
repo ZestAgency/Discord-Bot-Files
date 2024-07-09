d2af00afa9036f14f52b39c151cc19f8a3b24a22
ZestAgency <discord.strategyfr@gmail.com>
2024-07-09 20:43:08 +0200

    Delete Exemple 7.js

--- Exemple 7.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'help',
-    description: 'Liste des commandes disponibles',
-    execute(message, args) {
-        const commands = message.client.commands.map(cmd => cmd.name).join(', ');
-        message.channel.send(`Commandes disponibles : ${commands}`);
-    },
-};
